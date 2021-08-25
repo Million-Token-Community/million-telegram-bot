3dc2db07d9ff7fbdb10b76f17a58ce8d98b45dd0
Norrzzz <nope@norlunn.net>
2021-08-25 14:30:36 +0200

    Remove milliontoken.live key reference

--- includes/keys.php
+++ includes/keys.php
@@ -3,7 +3,6 @@
     # description / Keys used to authenticate to external APIs
 
     $apiToken = getenv("TELEGRAM_API_TOKEN");
-    $mtLiveKey = getenv("MILLIONTOKEN_LIVE_KEY");
     $ethScanKey = getenv("ETHSCAN_KEY");
     $nomicsKey = getenv("NOMICS_KEY");
     $covalentKey = getenv("COVALENT_KEY");
